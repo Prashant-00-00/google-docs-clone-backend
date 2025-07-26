04ddeb083bc0daae7525ffc704a621d35c395c47
Prashant-00-00 <sharma.prashant.8375@gmail.com>
2025-07-27 01:17:35 +0530

    Working backend with basic frontend

--- src/config/ws.config.ts
+++ src/config/ws.config.ts
@@ -1,17 +1,34 @@
-import { WebSocketServer } from 'ws'
-import * as Y from 'yjs'
-import * as http from 'http'
-import { getYDoc, setupWSConnection } from '../utils/y-websocket.js'
+// import { WebSocketServer } from 'ws'
+// import * as Y from 'yjs'
+// import * as http from 'http'
+// import { getYDoc, setupWSConnection } from '../utils/y-websocket.js'
 
-const docs = new Map<string, Y.Doc>()
+// const docs = new Map<string, Y.Doc>()
+
+// export function configureWebSocketServer(server: http.Server): void {
+//   const wss = new WebSocketServer({ server })
+
+//   wss.on('connection', (ws, req) => {
+//     const docName = req.url?.slice(1).split('?')[0] || 'default'
+//     const ydoc = getYDoc(docName)
+
+//     setupWSConnection(ws, req, { doc: ydoc })
+//   })
+// }
+// src/config/ws.config.ts
+// @ts-ignore
+import { setupWSConnection } from '../../node_modules/y-websocket/bin/utils.js';
+
+// import { setupWSConnection } from 'y-websocket/bin/utils.js'
+// import { setupWSConnection } from 'y-websocket/server'
+import { WebSocketServer } from 'ws'
+import * as http from 'http'
 
 export function configureWebSocketServer(server: http.Server): void {
   const wss = new WebSocketServer({ server })
 
   wss.on('connection', (ws, req) => {
-    const docName = req.url?.slice(1).split('?')[0] || 'default'
-    const ydoc = getYDoc(docName)
-
-    setupWSConnection(ws, req, { doc: ydoc })
+    setupWSConnection(ws, req)
   })
 }
+
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+// import express from 'express'
+// import cors from 'cors'
+// import http from 'http'
+// import { configureWebSocketServer } from './config/ws.config'
+
+// const app = express()
+// app.use(cors())
+
+// const server = http.createServer(app)
+// configureWebSocketServer(server)
+
+// const PORT = 8080
+// server.listen(PORT, () => {
+//   console.log(`🚀 Server listening at ${PORT}`)
+// })
+
 import express from 'express'
 import cors from 'cors'
 import http from 'http'
